5f7032e9be12ed5aa9abd8450f7a21e225c850a8
Stan Bad <stankv@mail.ru>
2023-04-11 14:46:39 +0300

    ООП: Итераторы и компараторы ч.1

---
+++ Lesson9_2/StudentsGroupIterator.java
@@ -0,0 +1,28 @@
+package Lesson9_2;
+
+import java.util.Iterator;
+import java.util.List;
+
+public class StudentsGroupIterator implements Iterator<Student> {
+    private int counter;
+    private List<Student> students;
+
+    public StudentsGroupIterator(List<Student> students) {
+        this.counter = 0;
+        this.students = students;
+    }
+
+
+    @Override
+    public boolean hasNext() {
+        return counter < students.size();
+    }
+
+    @Override
+    public Student next() {
+        if (!hasNext()) {
+            return null;
+        }
+        return students.get(counter++);
+    }
+}
